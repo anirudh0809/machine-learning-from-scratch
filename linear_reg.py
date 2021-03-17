8e14f7fba7d9131385ad747c6bbb0019f9c28cea
Anirudh Sharma <anirudhsharma@Anirudhs-MacBook-Pro.local>
2021-03-16 23:12:48 -0400

    first

---
+++ linear_reg.py
@@ -0,0 +1,24 @@
+import math
+
+class regression:
+    def __init__(self,X,y):
+        self.X = X
+        self.y=y
+
+    def data_loader(self):
+        pass
+
+    def forward(self):
+        pass
+    def step(self):
+        pass
+    def update_weights(self):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    if cross_val = True:
+        
+        
