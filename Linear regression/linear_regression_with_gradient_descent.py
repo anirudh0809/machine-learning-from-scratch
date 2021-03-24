52a2e2f30c69379bfb82063899e671476de0f387
Anirudh Sharma <anirudhsharma@Anirudhs-MacBook-Pro.local>
2021-03-24 15:03:12 -0400

    GDLR

---
+++ Linear regression/linear_regression_with_gradient_descent.py
@@ -0,0 +1,21 @@
+import numpy as np 
+import os
+import time 
+
+
+class gradient_regression():
+
+    def __init__(self):
+        pass 
+
+    def prediction(self,X,weight):
+        pass
+
+    def losses(self,prediction,y):
+        pass
+
+    def gradient_descent(self,weight,X,y,prediction):
+        pass
+
+    def main():
+        pass
