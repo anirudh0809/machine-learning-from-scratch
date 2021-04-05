78d6f4787ba2cd3dbb3d911953e9c6959d825c7a
anirudh0809 <anirudh4448@gmail.com>
2021-04-04 20:38:00 -0400

    init

---
+++ Regression/polynomial_reg.py
@@ -0,0 +1,16 @@
+
+import numpy as np 
+import sys 
+import scipy 
+from collections import OrderedDict
+
+class poly:
+    def __init__(self):
+        pass
+
+    def cost_computaion(self):
+        pass
+
+    def hypothesis_testing(self,theta,X):
+        pass
+    
